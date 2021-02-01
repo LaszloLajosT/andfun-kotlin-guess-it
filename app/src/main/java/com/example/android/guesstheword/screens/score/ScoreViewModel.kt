f93142f62bf3fc926ad819fd008e1b3178848a73
LaszloLajosT <track.runner800@outlook.com>
2021-02-01 19:58:31 +0000

    Update gradle

--- app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
@@ -18,5 +18,6 @@ package com.example.android.guesstheword.screens.score
 
 // TODO (01) Create the ScoreViewModel class and have it take in an integer constructor parameter
 // called finalScore
+
 // TODO (06) Add a LiveData for the score and the play again event, using the best practices for
 // encapsulation and event handling that you've learned
